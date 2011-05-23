4c7042720786742aed000a845917f516fd29c926
Daniel Hammond <daniel@twotoasters.com>
2011-05-23 11:03:37 -0400

    fixed spacing of badge images for label of flexible widths

--- Code/Views/GTIOOutfitTitleView.m
+++ Code/Views/GTIOOutfitTitleView.m
@@ -54,18 +54,27 @@
     //       that badgeview1 is always the leftmost for the label frame calculations -DRH
     CGFloat badgeTopPadding = 2.5;
     CGFloat badgeLeftPadding = (self.width - 20);
+    CGFloat badgeHorizontalPadding = 2;
     _badgeView1.frame = CGRectMake(badgeLeftPadding-_badgeView1.frame.size.width,badgeTopPadding,20,20);
-    _badgeView2.frame = CGRectMake(badgeLeftPadding,badgeTopPadding,20,20);
+    _badgeView2.frame = CGRectMake(badgeLeftPadding+badgeHorizontalPadding,badgeTopPadding,20,20);
     // Layout Labels Based on Badge Frames
     CGFloat nameLabelWidth = (_badgeView1.frame.origin.x - (self.width / 2)) * 2.0;     // (Distance of left most badge to center times 2)
     CGFloat nameLabelLeftPadding = (self.width - nameLabelWidth)/2.0;                   // Centered based on width
     _nameLabel.frame = CGRectMake(nameLabelLeftPadding, 4 - 2, nameLabelWidth, self.height * 0.65);
 	_locationLabel.frame = CGRectMake(0, self.height * 0.65 - 1, self.width, self.height * 0.35);
+    // Shift badges left if there is text in the namelabel and the size of that text is less than the label frames width
+    if (_nameLabel.text && ([_nameLabel.text sizeWithFont:_nameLabel.font].width < nameLabelWidth)) {
+        // shif the badges over by half the difference (label is centered)
+        CGFloat diff = (nameLabelWidth - [_nameLabel.text sizeWithFont:_nameLabel.font].width)/2.0-badgeHorizontalPadding;
+        _badgeView1.frame = CGRectMake(badgeLeftPadding-_badgeView1.frame.size.width-diff,badgeTopPadding,20,20);
+        _badgeView2.frame = CGRectMake(badgeLeftPadding-diff+badgeHorizontalPadding,badgeTopPadding,20,20);
+    }
 
 }
 
 - (void)setName:(NSString*)name {
 	_nameLabel.text = [[name copy] autorelease];
+    [self setNeedsLayout];
 }
 
 - (NSString*)name {
