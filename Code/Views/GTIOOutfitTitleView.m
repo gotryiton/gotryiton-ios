050cbbb3c85c83910806306a8beec7d0779d2d74
Daniel Hammond <daniel@twotoasters.com>
2011-05-23 10:43:15 -0400

    Updated badge assets, and setup custom outfit navigation bar background

--- Code/Views/GTIOOutfitTitleView.m
+++ Code/Views/GTIOOutfitTitleView.m
@@ -31,8 +31,8 @@
         // Badges
         _badgeView1 = [[UIImageView alloc] initWithFrame:CGRectMake(0,10,20,20)];
         _badgeView2 = [[UIImageView alloc] initWithFrame:CGRectMake(0,10,20,20)];
-        [_badgeView1 setImage:[UIImage imageNamed:@"exampleBadgeImage1.png"]];
-        [_badgeView2 setImage:[UIImage imageNamed:@"exampleBadgeImage2.png"]];
+        [_badgeView1 setImage:[UIImage imageNamed:@"outfit-badge-fashionista.png"]];
+        [_badgeView2 setImage:[UIImage imageNamed:@"outfit-badge-model.png"]];
         [self addSubview:_badgeView1];
         [self addSubview:_badgeView2];
 	}
@@ -51,7 +51,7 @@
     // Layout Badges
     // TODO: Once we have API integration we'll need to make this conditional based on number of badges
     //       We can safely assume according to GTIO team that there will be 0-2 badges, and here we assume 
-    //       that badgeview1 is always the leftmost for the label calculations -DRH
+    //       that badgeview1 is always the leftmost for the label frame calculations -DRH
     CGFloat badgeTopPadding = 2.5;
     CGFloat badgeLeftPadding = (self.width - 20);
     _badgeView1.frame = CGRectMake(badgeLeftPadding-_badgeView1.frame.size.width,badgeTopPadding,20,20);
