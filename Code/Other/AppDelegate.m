e1c95030529b076bac81474b197b8f6e60308da0
Jeremy Ellison <jeremy@twotoasters.com>
2011-08-23 14:11:15 -0400

    only show stylists push if stylistsCount is 0.

--- Code/Other/AppDelegate.m
+++ Code/Other/AppDelegate.m
@@ -521,7 +521,7 @@ void uncaughtExceptionHandler(NSException *exception) {
         TTOpenURL(@"gtio://home");
     }
     GTIOUser* user = [GTIOUser currentUser];
-    if (_showStylistPush && ![user.showAlmostDoneScreen boolValue]) {
+    if ([user.stylistsCount intValue] == 0 && _showStylistPush && ![user.showAlmostDoneScreen boolValue]) {
         // Wait for other navigations to finish
         [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.5]];
         TTOpenURL(@"gtio://pushStylists");
