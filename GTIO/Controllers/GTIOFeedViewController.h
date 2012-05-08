74f49acd51acfaa46604f174e0e54a81c992a026
Scott Penrose <penrose@gmail.com>
2012-05-08 16:08:59 -0400

    WIP: UITabBarController initial setup (Issue #48)

---
+++ GTIO/Controllers/GTIOFeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  GTIOFeedViewController.h
+//  GTIO
+//
+//  Created by Scott Penrose on 5/8/12.
+//  Copyright (c) 2012 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTIOFeedViewController : UIViewController
+
+@end
