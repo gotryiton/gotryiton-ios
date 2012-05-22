e7ab4b6a4bce073e48f1e3395e351388e250008c
Scott Penrose <penrose@gmail.com>
2012-05-22 14:49:42 -0400

    WIP: Initial camera displaying with AVFoundation (Issue #84)

--- GTIO/Controllers/GTIOCameraViewController.m
+++ GTIO/Controllers/GTIOCameraViewController.m
@@ -10,29 +10,65 @@
 
 @interface GTIOCameraViewController ()
 
+@property (nonatomic, strong) AVCaptureSession *session;
+
 @end
 
 @implementation GTIOCameraViewController
+
+@synthesize session = _session;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-
+        self.session = [[AVCaptureSession alloc] init];
+        self.session.sessionPreset = AVCaptureSessionPresetPhoto;
+        
+        NSArray *devices = [AVCaptureDevice devices];
+        AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        
+        NSError *error = nil;
+        AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+        if (!input) {
+            // Handle the error appropriately.
+            NSLog(@"Error: %@", [error localizedDescription]);
+        }
+        
+        [self.session beginConfiguration];
+        [self.session addInput:input];
+        [self.session commitConfiguration]; 
     }
     return self;
+}
+
+- (void)loadView
+{
+    self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
+    [self.view setAutoresizingMask:(UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight)];
+    [self.view setBackgroundColor:[UIColor blueColor]];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    // Setup preview
+    AVCaptureVideoPreviewLayer *captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.session];
+    [self.view.layer addSublayer:captureVideoPreviewLayer];
+    [self.session startRunning];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
