fbd5fe8ef6dcc7a958b5c5b34834578657e67efb
Scott Penrose <penrose@gmail.com>
2012-06-25 16:26:11 -0400

    Recreating GTIOPagination

---
+++ GTIO/Models/GTIOPagination.h
@@ -0,0 +1,16 @@
+//
+//  GTIOPagination.h
+//  GTIO
+//
+//  Created by Scott Penrose on 6/25/12.
+//  Copyright (c) 2012 Go Try It On. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTIOPagination : NSObject
+
+@property (nonatomic, strong) NSString *previousPage;
+@property (nonatomic, strong) NSString *nextPage;
+
+@end
---
+++ GTIO/Models/GTIOPagination.m
@@ -0,0 +1,15 @@
+//
+//  GTIOPagination.m
+//  GTIO
+//
+//  Created by Scott Penrose on 6/25/12.
+//  Copyright (c) 2012 Go Try It On. All rights reserved.
+//
+
+#import "GTIOPagination.h"
+
+@implementation GTIOPagination
+
+@synthesize previousPage = _previousPage, nextPage = _nextPage;
+
+@end
