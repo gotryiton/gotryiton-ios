267add6c64767bc34161498a9e530c949728c4c0
Scott Penrose <penrose@gmail.com>
2012-06-25 17:31:43 -0400

    Fixed tab bar image problem. Changed some strings to const.

--- GTIO/Application/GTIOAppDelegate.m
+++ GTIO/Application/GTIOAppDelegate.m
@@ -246,16 +246,23 @@
     [self.tab4ImageView setImage:[UIImage imageNamed:@"UI-Tab-4-OFF.png"]];
     [self.tab5ImageView setImage:[UIImage imageNamed:@"UI-Tab-5-OFF.png"]];
     
-    if ([viewController isKindOfClass:[GTIOFeedViewController class]]) {
-        [self.tab1ImageView setImage:[UIImage imageNamed:@"UI-Tab-1-ON.png"]];
+    if ([viewController isKindOfClass:[UINavigationController class]] && 
+        [((UINavigationController *)viewController).viewControllers count] > 0) {
+        
+        UINavigationController *navController = (UINavigationController *)viewController;
+        UIViewController *rootViewController = [navController.viewControllers objectAtIndex:0];
+        
+        if ([rootViewController isKindOfClass:[GTIOFeedViewController class]]) {
+            [self.tab1ImageView setImage:[UIImage imageNamed:@"UI-Tab-1-ON.png"]];
+        } else if ([rootViewController isKindOfClass:[GTIOMeViewController class]]) {
+            [self.tab5ImageView setImage:[UIImage imageNamed:@"UI-Tab-5-ON.png"]];
+        }
     } else if ([viewController isKindOfClass:[GTIOExploreLooksViewController class]]) {
         [self.tab2ImageView setImage:[UIImage imageNamed:@"UI-Tab-2-ON.png"]];
     } else if ([viewController isKindOfClass:[GTIOCameraViewController class]]) {
         [self.tab3ImageView setImage:[UIImage imageNamed:@"UI-Tab-3-ON.png"]];
     } else if ([viewController isKindOfClass:[GTIOShopViewController class]]) {
         [self.tab4ImageView setImage:[UIImage imageNamed:@"UI-Tab-4-ON.png"]];
-    } else if ([viewController isKindOfClass:[UINavigationController class]]) {
-        [self.tab5ImageView setImage:[UIImage imageNamed:@"UI-Tab-5-ON.png"]];
     }
 }
 
