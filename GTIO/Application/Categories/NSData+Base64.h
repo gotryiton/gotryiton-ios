e6f2bd24ab9dcc7c88c6e3e23545b37d3ed53ee5
Scott Penrose <penrose@gmail.com>
2012-07-13 18:42:55 -0400

    Fixed more janrain. Removed base64 category because it wasn't work on iphone. Rev to version 4.0.0.11.

--- GTIO/Application/Categories/NSData+Base64.h
+++
@@ -1,8 +0,0 @@
-// http://www.cocoadev.com/index.pl?BaseSixtyFour
-
-@interface NSData (Base64)
-
-+ (id)gtio_dataWithBase64EncodedString:(NSString *)string;     //  Padding '=' characters are optional. Whitespace is ignored.
-- (NSString *)gtio_base64Encoding;
-
-@end
