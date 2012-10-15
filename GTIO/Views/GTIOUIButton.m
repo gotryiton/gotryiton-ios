a912ebe3f4f6f2c0934703a73872257a0cbdaec0
Simon Holroyd <sholroyd@gmail.com>
2012-10-15 17:43:38 -0400

    fix for #567

--- GTIO/Views/GTIOUIButton.m
+++ GTIO/Views/GTIOUIButton.m
@@ -640,7 +640,8 @@ static CGFloat const kGTIOSpinnerTopPadding = 2.0;
 + (id)gtio_productShoppingListNavButton
 {
     GTIOUIButton *button = [self buttonWithImage:[UIImage imageNamed:@"nav.bar.dot.inactive.png"] hightlightImage:[UIImage imageNamed:@"nav.bar.dot.active.png"]];
-    [button.imageView setFrame:(CGRect){ button.bounds.origin.x, button.bounds.origin.y+2, button.bounds.size.width, button.bounds.size.height	 }];
+    [button setImageEdgeInsets:(UIEdgeInsets){2, 0, 0, 0}];
+    [button.imageView setFrame:(CGRect){ button.bounds.origin.x, button.bounds.origin.y, button.bounds.size.width, button.bounds.size.height	 }];
     return button;
 }
 
